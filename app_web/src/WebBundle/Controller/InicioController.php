36603f21f265d3b14bb95424ca41603b1f8d0f40
Jonathan Padilla <jonathanpadilla09@outlook.com>
2016-09-26 19:03:16 -0400

    editar textos listo

--- app_web/src/WebBundle/Controller/InicioController.php
+++ app_web/src/WebBundle/Controller/InicioController.php
@@ -3,6 +3,7 @@
 namespace WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class InicioController extends Controller
 {
@@ -14,5 +15,36 @@ class InicioController extends Controller
         	'pagina' => 'inicio',
         	'session' => $session
         	));
+    }
+
+    // FUNCIONES AJAX
+    public function guardarTextoAction(Request $request)
+    {
+    	// variables
+    	$result = false;
+    	$id 	= ($request->get('id', false))		?$request->get('id')	:null;
+    	$campo 	= ($request->get('campo', false))	?$request->get('campo')	:null;
+    	$texto 	= ($request->get('texto', false))	?$request->get('texto')	:null;
+    	$em 	= $this->getDoctrine()->getManager();
+
+    	// actualizar texto
+    	if( is_numeric($id) && $seccion = $em->getRepository('WebBundle:Seccion')->findOneBy(array('id' => $id )) )
+    	{
+    		switch($campo)
+    		{
+    			case 1: $seccion->setTexto1($texto);break;
+    			case 2: $seccion->setTexto2($texto);break;
+    			case 3: $seccion->setTexto3($texto);break;
+    			case 4: $seccion->setTexto4($texto);break;
+    		}
+
+    		$em->persist($seccion);
+    		$em->flush();
+
+    		$result = true;
+    	}
+
+    	echo json_encode(['result' => $result]);
+    	exit;
     }
 }
--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,101 @@
+var btn_inicio_editar_bienvenida 	= $("#btn_inicio_editar_bienvenida");
+
+var editar_texto_input 				= $(".editar_texto_input");
+var editar_texto_area 				= $(".editar_texto_area");
+
 $(function(){
 	'use strict';
 
-	$(".btn-editar").hover(function(){
-		console.log('hover');
+	// inputs
+	editar_texto_input.dblclick(function(){
+
+		// variables
+		var content = $(this);
+		var item 	= content.data('item');
+		var texto 	= content.find(item).text();
+		var id 		= content.data('id');
+		var campo 	= content.data('campo');
+
+		// crear input o textarea
+		var input = focusInTexto( content, texto, 'input');
+		
+		// destruir textarea y enviar para guardar
+		content.focusout(function(){
+
+			var texto_final = focusOutTexto(content, input, item)
+			guardarTexto(texto_final, id, campo);
+
+		});
+
+	});
+
+	// textarea
+	editar_texto_area.dblclick(function(){
+
+		// variables
+		var content = $(this);
+		var item 	= content.data('item');
+		var texto 	= content.find(item).text();
+		var id 		= content.data('id');
+		var campo 	= content.data('campo');
+
+		// crear input o textarea
+		var input = focusInTexto( content, texto, 'textarea');
+		
+		// destruir textarea y enviar para guardar
+		content.focusout(function(){
+
+			var texto_final = focusOutTexto(content, input, item)
+			guardarTexto(texto_final, id, campo);
+
+		});
+
 	});
 
 });
+
+function focusInTexto(content, texto, area)
+{
+	// input o textarea
+	var campo_texto = '';
+	if(area == 'input')
+		campo_texto = '<input type="text" id="texto_generico" value="'+texto+'">';
+	else if(area == 'textarea')
+		campo_texto = '<textarea id="texto_generico" rows="5">'+texto+'</textarea>';
+
+	// insertar en el contenido y agregar focus
+	content.html(campo_texto);
+	$("#texto_generico").focus();
+
+	return $("#texto_generico");
+}
+
+function focusOutTexto(content, texto, item)
+{
+	var texto_real = texto.val();
+	content.html('<'+item+'>'+texto_real+'</'+item+'>');
+	return texto_real;
+}
+
+function guardarTexto(texto, id, campo)
+{
+	var arr = {
+		'texto': texto,
+		'id': id,
+		'campo': campo
+	}
+
+	$.ajax({
+		url: Routing.generate('ajax_guardar_texto'),
+		data: arr,
+		dataType: 'json',
+		method: 'post',
+	}).success(function(json){
+		if(json.result)
+		{
+			console.log(json);
+		}
+	
+	});
+	
+}
